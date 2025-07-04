ffbad072996f53661f54d85bbb4fe9cfa4f4b186
JYOTIPRAKASH PANDA <112711109+iamjyotiprakash2002@users.noreply.github.com>
2025-07-04 22:26:33 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -144,4 +144,6 @@ if __name__ == '__main__':
     t.start()
 
     print("[*] Running Flask server at http://localhost:5000")
-    socketio.run(app, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
+
